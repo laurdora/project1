cbea47f060a0d65504f886fcbe4d2fab9335f48d
s3503177 <s3503177@student.rmit.edu.au>
2017-04-26 21:53:02 +1000

    Added Search bar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,7 +14,7 @@
   <div class="container-fluid">
     <div></div> <div class="navbar-header">
       <a style='font-size:xx-large' href="#" class="navbar-brand">Project1</a>
-  </div>
+                 </div>
   <!--<ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
       <li><a href="#">Page 1</a></li>
@@ -22,10 +22,27 @@
       <li><a href="#">Page 3</a></li>
     </ul> -->
     @if(Auth::user())
-        <ul class="nav navbar-nav navbar-right">
-      <li><a href="{{URL::to('/logout')}}">
-      <span>{{ucwords(Auth::user()->fname)}}</span>
-      <span class="glyphicon glyphicon-log-in"></span> Sign out</a></li>
+
+    
+<form action="/search" method="POST" role="search">
+    {{ csrf_field() }}
+    <div class="input-group">
+        <input type="text" class="form-control" name="q"
+            placeholder="Search users"> <span class="input-group-btn">
+            <button type="submit" class="btn btn-default">
+                <span class="glyphicon glyphicon-search"></span>
+            </button>
+        </span>
+    </div>
+</form>
+
+
+    <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="{{URL::to('/upload')}}">Create new article</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+        <li><a href="{{URL::to('/logout')}}">
+        <span>{{ucwords(Auth::user()->fname)}}</span>
+        <span class="glyphicon glyphicon-log-in"></span> Sign out</a></li>
     </ul>
     @endif
 
